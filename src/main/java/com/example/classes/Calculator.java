7036aac3da238418a493b30aad6559cd45800538
Miruna Landstrom <mirunalandstrom@hotmail.com>
2017-11-14 11:29:51 +0100

    This is my first version

---
+++ src/main/java/com/example/classes/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.classes;
+
+public class Calculator {
+	public double addition(double firstNumber, double secondNumber) {
+		double result = firstNumber + secondNumber;
+		return result;
+	}
+	public double substraction(double firstNumber, double secondNumber) {
+		double result = firstNumber - secondNumber ;
+		return result;
+		
+	}
+	public double division(double firstNumber, double secondNumber) {
+		double result = firstNumber/secondNumber;
+		return result;
+		
+	}
+	public double multiplication(double firstNumber, double secondNumber) {
+		double result = firstNumber*secondNumber;
+		return result;
+		
+	}
+}
---
+++ src/main/java/com/example/main/Main.java
@@ -0,0 +1,18 @@
+package com.example.main;
+
+import com.example.classes.Calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		System.out.println(calculator.addition(2, 2));
+		System.out.println(calculator.substraction(2,2));
+		System.out.println(calculator.division(2, 2));
+		System.out.println(calculator.multiplication(2, 2));
+		
+
+	}
+
+}
